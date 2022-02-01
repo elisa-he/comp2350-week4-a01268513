988ffcad2679fc23c8252e059d5b4a0618a52d34
elisa-he <97122439+elisa-he@users.noreply.github.com>
2022-02-01 00:35:31 -0800

    Update databaseConnection.js

--- databaseConnection.js
+++ databaseConnection.js
@@ -5,10 +5,10 @@ const is_heroku = process.env.IS_HEROKU || false;
 
 //mysql://b6fb24cb4c8c37:702be0f5@us-cdbr-east-03.cleardb.com/heroku_a79dfbc72e06757?reconnect=true
 const dbConfigHeroku = {
-	host: "us-cdbr-east-03.cleardb.com",
-	user: "b6fb24cb4c8c37",
-	password: "702be0f5",
-	database: "heroku_a79dfbc72e06757",
+	host: "qao3ibsa7hhgecbv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+	user: "xmymwil64iqqnb97",
+	password: "xzil4inuhju2qg5t",
+	database: "sps2kx7fb0h92fir",
 	multipleStatements: false
 };
 
